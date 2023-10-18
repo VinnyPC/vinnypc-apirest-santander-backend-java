6918b26dd2526ede75f65838318bee70673bf599
VinnyPC <vinisilvax3@gmail.com>
2023-10-18 20:50:11 -0300

    Initial commit

---
+++ src/main/java/com/vinnypc/VinnypcApirestApplication.java
@@ -0,0 +1,13 @@
+package com.vinnypc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinnypcApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinnypcApirestApplication.class, args);
+	}
+
+}
